6c4d52ce2e982bd47e9c1e69e35c3813d000b2d7
kjuulh <contact@kjuulh.io>
2022-06-09 23:13:10 +0200

    added full stack lib test

--- internal/lib_test.go
+++ internal/lib_test.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/kjuulh/stiletto/pkg/client"
 	"github.com/kjuulh/stiletto/pkg/featurestores"
 	"github.com/stretchr/testify/require"
@@ -24,4 +26,42 @@ func TestIsEntry(t *testing.T) {
 	isNotEntry, err := uut.GetFeature(context.Background(), "user", "not a correct user id")
 	require.NoError(t, err)
 	require.False(t, isNotEntry)
+}
+
+func TestStack(t *testing.T) {
+	uut := client.NewStilettoClient()
+
+	httpClient := &featurestores.HttpClientMock{GetFunc: func(s string) ([]byte, error) {
+		if s == "http://localhost/keys/some-key" {
+			return json.Marshal(featurestores.Feature{
+				Key:     "some-key",
+				Enabled: true,
+			})
+		}
+		if s == "http://localhost/keys/some-other-key" {
+			return json.Marshal(featurestores.Feature{
+				Key:     "some-other-key",
+				Enabled: false,
+			})
+		}
+		return nil, fmt.Errorf("query does not match aborting, %s", s)
+	}}
+
+	uut.SetFeatureStore(
+		"user",
+		featurestores.NewEagerFeatureStore(
+			featurestores.NewCache(
+				featurestores.
+					NewRemote("http://localhost/keys").
+					WithHttpClient(httpClient).
+					Build()),
+			[]string{"some-key", "some-other-key", "some-key"},
+		),
+	)
+
+	feature, err := uut.GetFeature(context.Background(), "user", "some-key")
+	require.NoError(t, err)
+	require.True(t, feature)
+
+	require.Equal(t, 2, len(httpClient.GetCalls()))
 }
